a211be940361f8539632eb3e9265a325bb492af3
jartek <jartek.91@gmail.com>
2017-08-01 12:08:43 +0530

    feat(app): Add firebase support

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const rp = require('request-promise');
 const http = require('http');
 const randomColor = require('randomcolor');
-
-require('dotenv').config();
-require('http');
+const firebase = require("firebase");
 
 const app = express();
 const urlencodedParser = bodyParser.urlencoded();
 const jsonParser = bodyParser.json();
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_PROJECT_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID
+};
+
+firebase.initializeApp(config);
+const storage = firebase.storage();
+const database = firebase.database();
 
 app.post('/travis', urlencodedParser, (req, res, next) => {});
 
